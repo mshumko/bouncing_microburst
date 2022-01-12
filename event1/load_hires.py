839fe7dd33ddf968fd4a9bf22285bbbc3a7e1de5
Mykhaylo Shumko <msshumko@gmail.com>
2022-01-12 09:35:49 -0500

    Finished writing the HiRes data reader.

--- event1/load_hires.py
+++ event1/load_hires.py
@@ -33,12 +33,56 @@ def readJSONheadedASCII(file_path):
             if line.startswith('#'):
                 header_list.append(line[1:])
             else:
-                data = f.readlines()
-                
+                raw_data_str = f.readlines()
+
+    # Massage the header
     clean_header_str = ''.join(header_list).replace('\n', '')
     parsed_header = json.loads(clean_header_str)
+    # Massage the data
+    raw_data_str = [row.replace('\n', '') for row in raw_data_str]
+    # Parse times
+    times_str = [row.split()[0] for row in raw_data_str]
+    # Parse the other columns
+    data_converted = np.array([row.split()[1:] for row in raw_data_str]).astype(float)
 
-    return
+    data = HiRes()
+    data['Time'] = pd.to_datetime(times_str)
+    for key in parsed_header:
+        if key == 'Time':
+            continue
+        key_header = parsed_header[key]
+        # column attributes
+        if isinstance(key_header, dict):
+            print(key_header['DIMENSION'])
+            start_column = key_header['START_COLUMN']-1
+            end_column = key_header['START_COLUMN']-1+key_header['DIMENSION'][0]
+            if key_header['DIMENSION'][0] == 1:
+                data[key] = data_converted[:, start_column]
+            else:
+                data[key] = data_converted[:, start_column:end_column]
+            
+            data.attrs[key] = key_header
+        else:
+            # Global attributes
+            if key in ['CADENCE', 'CAMPAIGN']:
+                data.attrs[key] = float(key_header)
+            else:
+                data.attrs[key] = key_header
+        
+    return data
+
+class HiRes(dict):
+    """
+    Expand Python's dict class to include an attr attribute dictionary.
+    
+    Code credit goes to Matt Anderson:
+    https://stackoverflow.com/questions/2390827/how-to-properly-subclass-dict-and-override-getitem-setitem
+    (blame him for problems)
+    """
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+        self.attrs = {}
+        return
 
 if __name__ == '__main__':
     hr = load_hires(4, datetime(2015, 5, 25))
