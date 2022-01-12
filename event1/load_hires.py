36d592a8a77884a1b66808cacbbc504cfe262601
Mykhaylo Shumko <msshumko@gmail.com>
2022-01-12 09:04:01 -0500

    Writing the header parser.

--- event1/load_hires.py
+++ event1/load_hires.py
@@ -12,9 +12,9 @@ def load_hires(sc_id, date):
     Finds and loads the FIREBIRD HiRes data.
     """
     search_str = f'FU{sc_id}_Hires_{date.strftime("%Y-%m-%d")}_L2.txt'
-    hr_paths = sorted(pathlib.Path(config.FB_DIR).rglob(search_str))
+    hr_paths = sorted(pathlib.Path(config['FB_DIR']).rglob(search_str))
     assert len(hr_paths) == 1, (
-        f'{len(hr_paths)} HiRes paths found in {pathlib.Path(config.FB_DIR)} '
+        f'{len(hr_paths)} HiRes paths found in {pathlib.Path(config["FB_DIR"])} '
         f'that match {search_str}.'
         )
     hr = readJSONheadedASCII(str(hr_paths[0]))
@@ -27,15 +27,16 @@ def readJSONheadedASCII(file_path):
     (for some who) have difficulty installing it.
     """
     # Read in the JSON header.
-    header = []
+    header_list = []
     with open(file_path, 'r') as f:
         for line in f:
             if line.startswith('#'):
-                header.append(line[1:])
+                header_list.append(line[1:])
             else:
                 data = f.readlines()
-
-    parsed_header = json.loads(header)
+                
+    clean_header_str = ''.join(header_list).replace('\n', '')
+    parsed_header = json.loads(clean_header_str)
 
     return
 
