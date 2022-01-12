79364f1268d69947b589c7176616c56fc917ed47
Mykhaylo Shumko <msshumko@gmail.com>
2022-01-12 11:22:40 -0500

    Added the plot labels.

--- event1/bounce_period.py
+++ event1/bounce_period.py
@@ -6,13 +6,16 @@ import IRBEM
 
 from event1.load_hires import load_hires
 
-time = datetime(2015, 5, 25, 18, 18, 52)
-hr = load_hires(4, time)
-idt = np.where(hr['Time'] > time)[0][0]
+def bounce_period(sc_id, time):
+    hr = load_hires(4, time)
+    idt = np.where(hr['Time'] > time)[0][0]
 
-X = {'Time':hr['Time'][idt], 'x1':hr['Alt'][idt], 'x2':hr['Lat'][idt], 'x3':hr['Lon'][idt]}
-maginput = {'Kp':hr['kp'][idt]}
+    X = {'Time':hr['Time'][idt], 'x1':hr['Alt'][idt], 'x2':hr['Lat'][idt], 'x3':hr['Lon'][idt]}
+    maginput = {'Kp':hr['kp'][idt]}
 
-m = IRBEM.MagFields(kext='T89')
-tb = m.bounce_period(X, maginput, 200)
-print(f'The bounce period is {tb} seconds.')
+    m = IRBEM.MagFields(kext='T89')
+    return m.bounce_period(X, maginput, 200)
+
+if __name__ == '__main__':
+    tb = bounce_period(4, datetime(2015, 5, 25, 18, 18, 52))
+    print(f'The bounce period is {tb} seconds.')
--- event1/spectrum.py
+++ event1/spectrum.py
@@ -7,27 +7,34 @@ import matplotlib.ticker
 import matplotlib.dates
 
 from event1.load_hires import load_hires
+from event1.bounce_period import bounce_period
 
 time_range = [
     datetime(2015, 5, 25, 18, 18, 47),
     datetime(2015, 5, 25, 18, 18, 58)
 ]
+
+tb = bounce_period(4, time_range[0])
+
 hr = load_hires(4, time_range[0])
 idt = np.where(
     (hr['Time'] >= time_range[0]) &
     (hr['Time'] <= time_range[1])
     )[0]
 
-hr['Time'] = hr['Time'][idt]
+for key in hr:
+    if len(hr[key].shape) == 1:
+        hr[key] = hr[key][idt]
 hr['Col_counts'] = hr['Col_counts'][idt, 0]
 fig, ax = plt.subplots(2, sharex=True)
 
 ax[0].plot(hr['Time'], hr['Col_counts'], 'k')
-# ax[0].set_xlim(time_range)
-locator=matplotlib.ticker.MaxNLocator(nbins=5)
-ax[-1].xaxis.set_major_locator(locator)
-fmt = matplotlib.dates.DateFormatter('%H:%M:%S')
-ax[-1].xaxis.set_major_formatter(fmt)
+s = (
+    f'L={round(hr["McIlwainL"][0], 1)}\n'
+    f'MLT={round(hr["MLT"][0], 1)}\n'
+    f'(lat,lon)=({round(hr["Lat"][0], 1)}, {round(hr["Lon"][0], 1)})'
+    )
+ax[0].text(0.7, 1, s, va='top', transform=ax[0].transAxes)
 
 freqs, times, spectrogram = scipy.signal.spectrogram(
     hr['Col_counts'], fs=1/hr.attrs['CADENCE'], 
@@ -37,5 +44,20 @@ p = ax[1].pcolormesh(
     spectrogram_times, freqs, spectrogram[:-1, :-1], 
     norm=matplotlib.colors.LogNorm(vmin=None, vmax=None)
     )
+ax[1].text(spectrogram_times[0], 1/tb+0.5, f'Modeled $f_b$={round(1/tb, 1)}', 
+    c='white', fontsize=15)    
+ax[1].axhline(1/tb, c='w')
+ax[1].text(spectrogram_times[0], 2/tb+0.5, f'2$\times f_b$', 
+    c='white', fontsize=15)
+ax[1].axhline(2/tb, c='w', ls=':')
+ax[1].text(spectrogram_times[0], 3/tb+0.5, f'3$f_b$', 
+    c='white', fontsize=15)
+ax[1].axhline(3/tb, c='w', ls=':')
+
+locator=matplotlib.ticker.MaxNLocator(nbins=5)
+ax[-1].xaxis.set_major_locator(locator)
+fmt = matplotlib.dates.DateFormatter('%H:%M:%S')
+ax[-1].xaxis.set_major_formatter(fmt)
+
 plt.tight_layout()
 plt.show()
